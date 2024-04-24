c9d6801d3e06ed80cd6b204832b37636cfa9cb94
Matheus Benatti <matheus.cwb2@gmail.com>
2024-04-24 20:22:07 -0300

    Adição função de adicionar linha na tabela

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,11 +4,39 @@ function adicionaDadosAluno(){
     const nome = document.getElementById("input_nome").value;
     const ra = document.getElementById("input_ra").value;
     const email = document.getElementById("input_email").value;
-    const aluno = new Aluno(nome, ra, email);
+    // const aluno = new Aluno(nome, ra, email);
+    const aluno = {
+      nome: nome,
+      ra: ra,
+      email: email
+  };
     alunos.push(aluno);
+    adicionarLinha(aluno);
 }
 
 function excluirlinha(botao){
 let linha = botao.parentNode.parentNode;     //<button onclick="excluirLinha(this)">Excluir</button>
   linha.parentNode.removeChild(linha); 
+}
+
+function adicionarLinha(aluno){
+    let novaLinha = document.createElement('tr');
+
+    let nome = document.createElement('td');
+    nome.innerHTML = aluno.nome;
+    novaLinha.appendChild(nome);
+
+    let RA = document.createElement('td');
+    RA.innerHTML = aluno.ra;
+    novaLinha.appendChild(RA);
+
+    let Email = document.createElement('td');
+    Email.innerHTML = aluno.email;
+    novaLinha.appendChild(Email);
+
+    document.getElementById('idTabela').appendChild(novaLinha);
+}
+
+function json(){
+    return JSON.stringify(alunos);
 }
