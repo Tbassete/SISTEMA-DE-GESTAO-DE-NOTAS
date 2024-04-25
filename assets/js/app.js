efae404eca3ac48c8f251893f27ec8073640b730
Thiago Bassete <taraujo438.ta@gmail.com>
2024-04-24 21:26:15 -0300

    tratamento de dados no formulario

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,6 +13,10 @@ function adicionaDadosAluno(){
     const ra = document.getElementById("input_ra").value;
     const email = document.getElementById("input_email").value;
 
+    if (!nome || !ra || !email ) {
+        alert("Todos os campos são obrigatórios!");
+        return;
+    }
 
     const aluno = {
         nome: nome,
@@ -28,9 +32,36 @@ function adicionaDadosAluno(){
     localStorage.setItem('aluno', JSON.stringify(alunos));
 
     alert("Aluno adicionado com sucesso!");
+
+    const tabela = document.getElementById('idTabela');
+
+    alunos.forEach(aluno => {
+        // Cria uma nova linha na tabela
+        const novaLinha = document.createElement('tr');
+    
+        // Adiciona as células com os dados do aluno à nova linha
+        novaLinha.innerHTML = `
+            <td>${aluno.nome}</td>
+            <td>${aluno.ra}</td>
+            <td>${aluno.email}</td>
+            <td>${aluno.prova1}</td>
+            <td>${aluno.prova_integrada1}</td>
+            <td>${aluno.media1Bimestre || '-'}</td>
+            <td>${aluno.prova2 || '-'}</td>
+            <td>${aluno.aep2 || '-'}</td>
+            <td>${aluno.prova_integrada2 || '-'}</td>
+            <td>${aluno.media2Bimestre || '-'}</td>
+            <td>${aluno.mediaFinal || '-'}</td>
+            <td>${aluno.situacao || '-'}</td>
+        `;
+    
+        // Adiciona a nova linha à tabela
+        tabela.appendChild(novaLinha);
+    });
 }
 
-function adicionanNota1(){
+
+function adicionaNota1B(){
 
     const prova1 = document.getElementById("input_prova_1").value;
     const prova_integrada1 = document.getElementById("input_prova_integrada_1").value;
