a4dda45b92b841f9213157ffd408f8eb51b8cc6f
Matheus Benatti <matheus.cwb2@gmail.com>
2024-04-27 15:52:30 -0300

    Atualização funções de média, tratamento de erros

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,31 +3,57 @@
 exibirTabelaAlunos();
 atualizarMediasNaTabela();
 
-
 let idFormulario = document.getElementById("idFormulario");
 
 idFormulario.addEventListener("submit", (e) => {
     e.preventDefault();
 })
+function validaRA(ra, alunos) {
+
+    //let alunos = JSON.parse(localStorage.getItem('aluno')) || [];
+    for (let i = 0; i < alunos.length; i++) {
+        if (alunos[i].ra === ra) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function validarNota(nota) {
+    // Se a nota for null ou uma string vazia, considera como 0
+    if (nota === null || nota === "" || nota === "-" || nota == undefined) {
+        return true;
+    }
+
+    // Convertendo a string para número antes de validar
+    const notaNum = parseFloat(nota);
+    return !isNaN(notaNum) && notaNum >= 0 && notaNum <= 10;
+}
 
 function adicionaDadosAluno() {
     const nome = document.getElementById("input_nome").value;
     const ra = document.getElementById("input_ra").value;
     const email = document.getElementById("input_email").value;
-    let prova1 = 0;
-    let aep1 = 0;
-    let prova_integrada1 = 0;
-    let prova2 = 0;
-    let aep2 = 0;
-    let prova_integrada2 = 0;
-    let media1 = 0;
-    let media2 = 0;
-    let final = 0;
-    situacao = "";
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+    let prova1 = null;
+    let aep1 = null;
+    let prova_integrada1 = null;
+    let prova2 = null;
+    let aep2 = null;
+    let prova_integrada2 = null;
+    let media1 = null;
+    let media2 = null;
+    let final = null;
+    let situacao = "";
+    const nomeRegex = /^[A-Za-zÀ-ÖØ-öø-ÿ\s]+$/; // Permite letras (com ou sem acento) e espaços
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
     if (!nome || !ra || !email) {
         alert("Todos os campos são obrigatórios!");
+        return;
+    }
+
+    if (!nomeRegex.test(nome)) {
+        alert("O nome inserido não é válido! Por favor, insira apenas letras.");
         return;
     }
 
@@ -36,23 +62,29 @@ function adicionaDadosAluno() {
         return;
     }
 
+    let alunos = JSON.parse(localStorage.getItem('aluno')) || [];
+
+    if (validaRA(ra, alunos)) {
+        alert("Este RA já foi cadastrado. Por favor, insira um RA único.");
+        return;
+    }
+
     const aluno = {
         nome: nome,
         ra: ra,
         email: email,
-        prova1 : prova1,
-        aep1 : aep1,
-        prova_integrada1 : prova_integrada1,
-        prova2 : prova2,
-        aep2 : aep2,
-        prova_integrada2 : prova_integrada2,
-        media1 : media1,
-        media2 : media2,
-        final : final,
+        prova1: prova1,
+        aep1: aep1,
+        prova_integrada1: prova_integrada1,
+        prova2: prova2,
+        aep2: aep2,
+        prova_integrada2: prova_integrada2,
+        media1: media1,
+        media2: media2,
+        final: final,
         situacao: situacao
     };
 
-    let alunos = JSON.parse(localStorage.getItem('aluno')) || [];
     alunos.push(aluno);
     localStorage.setItem('aluno', JSON.stringify(alunos));
 
@@ -72,25 +104,25 @@ function adicionaDadosAluno() {
     calcularMediaTotalFinal();
 }
 
-function adicionarNotasAluno(aluno) {
-
-    const prova1 = document.getElementById("input_prova_1").value;
-    const aep1 = document.getElementById("input_aep_1").value;
-    const prova_integrada1 = document.getElementById("input_prova_integrada_1").value;
-    const prova2 = document.getElementById("input_prova_2").value;
-    const aep2 = document.getElementById("input_aep_2").value;
-    const prova_integrada2 = document.getElementById("input_prova_integrada_2").value;
-
+function adicionarNotasAluno(aluno, prova1, aep1, integrada1, prova2, aep2, integrada2) {
+    if (!validarNota(prova1) || !validarNota(aep1) || !validarNota(integrada1) ||
+        !validarNota(prova2) || !validarNota(aep2) || !validarNota(integrada2)) {
+        alert("As notas devem ser um número entre 0.0 e 10.0.");
+        return;
+    }
 
     aluno.prova1 = prova1;
     aluno.aep1 = aep1;
-    aluno.prova_integrada1 = prova_integrada1;
+    aluno.prova_integrada1 = integrada1;
     aluno.prova2 = prova2;
     aluno.aep2 = aep2;
-    aluno.prova_integrada2 = prova_integrada2;
+    aluno.prova_integrada2 = integrada2;
 
-    aluno.media1 = calcularMedia1(aluno);
-    aluno.media2 = calcularMedia2(aluno);
+    let media1 = calcularMedia1(aluno) || 0;
+    let media2 = calcularMedia2(aluno) || 0;
+
+    aluno.media1 = media1;
+    aluno.media2 = media2;
     aluno.final = calcularMediaFinal(aluno);
     aluno.situacao = definirSituacao(aluno);
 
@@ -104,6 +136,23 @@ function adicionarNotasAluno(aluno) {
     } else {
         alert("Aluno não encontrado. Certifique-se de que o RA está correto.");
     }
+
+    // Atualizar a linha na tabela com as novas notas
+    const linha = document.getElementById('linha' + aluno.ra);
+    if (linha) {
+        linha.cells[3].textContent = prova1;
+        linha.cells[4].textContent = aep1;
+        linha.cells[5].textContent = integrada1;
+        linha.cells[7].textContent = prova2;
+        linha.cells[8].textContent = aep2;
+        linha.cells[9].textContent = integrada2;
+        linha.cells[6].textContent = aluno.media1;
+        linha.cells[10].textContent = aluno.media2;
+        linha.cells[11].textContent = aluno.final;
+        linha.cells[12].textContent = aluno.situacao;
+    }
+
+    // Chamar as funções para recalcular as médias totais
     calcularMediaTotalProva1();
     calcularMedia1Total();
     calcularMediaTotalAEP1();
@@ -113,12 +162,11 @@ function adicionarNotasAluno(aluno) {
     calcularMediaTotalAEP2();
     calcularMediaTotalIntegrada2();
     calcularMediaTotalFinal();
-
 }
 
 function adicionarLinha(aluno) {
     let idLinha = 'linha' + Date.now();
-    
+
     let novaLinha = document.createElement('tr');
     novaLinha.id = idLinha;
     function adicionarCelula(valor) {
@@ -133,13 +181,13 @@ function adicionarLinha(aluno) {
     adicionarCelula(aluno.prova1);
     adicionarCelula(aluno.aep1);
     adicionarCelula(aluno.prova_integrada1);
-    adicionarCelula(calcularMedia1(aluno));
+    adicionarCelula(aluno.media1);
     adicionarCelula(aluno.prova2);
     adicionarCelula(aluno.aep2);
     adicionarCelula(aluno.prova_integrada2);
-    adicionarCelula(calcularMedia2(aluno));
-    adicionarCelula(calcularMediaFinal(aluno));
-    adicionarCelula(definirSituacao(aluno));
+    adicionarCelula(aluno.media2);
+    adicionarCelula(aluno.final);
+    adicionarCelula(aluno.situacao);
 
     let celulaAcoes = document.createElement('td');
     celulaAcoes.classList.add('actions');
@@ -161,13 +209,13 @@ function exibirTabelaAlunos() {
     const alunos = JSON.parse(localStorage.getItem('aluno')) || [];
 
     alunos.forEach(aluno => {
-        if (aluno.ra == ""){
+        if (aluno.ra == "") {
             excluirLinha(aluno.ra);
-        } else{
-        let idLinha = 'linha' + Date.now();
-        let novaLinha = document.createElement('tr');
-        novaLinha.id = idLinha;
-        novaLinha.innerHTML = `
+        } else {
+            let idLinha = 'linha' + Date.now();
+            let novaLinha = document.createElement('tr');
+            novaLinha.id = idLinha;
+            novaLinha.innerHTML = `
             <td>${aluno.nome || '-'}</td>
             <td>${aluno.ra || '-'}</td>
             <td>${aluno.email || '-'}</td>
@@ -187,8 +235,9 @@ function exibirTabelaAlunos() {
                 <button class="delete" onclick="excluirLinha('${aluno.ra}')">Excluir</button>
             </td>
         `;
-        tabela.appendChild(novaLinha);
-    }});
+            tabela.appendChild(novaLinha);
+        }
+    });
 
     // tabelaExibida = true;
     calcularMediaTotalProva1();
@@ -252,70 +301,64 @@ function editarDados(botaoEditar) {
 function editarNotas(botaoAdd) {
     let linha = botaoAdd.parentNode.parentNode;
 
-    let prova1 = linha.cells[3].innerHTML;
-    let aep1 = linha.cells[4].innerHTML;
-    let integrada1 = linha.cells[5].innerHTML;
-    let prova2 = linha.cells[6].innerHTML;
-    let aep2 = linha.cells[7].innerHTML;
-    let integrada2 = linha.cells[8].innerHTML;
+    let prova1 = linha.cells[3].innerHTML || '0';
+    let aep1 = linha.cells[4].innerHTML || '0';
+    let integrada1 = linha.cells[5].innerHTML || '0';
+    let prova2 = linha.cells[7].innerHTML || '0';
+    let aep2 = linha.cells[8].innerHTML || '0';
+    let integrada2 = linha.cells[9].innerHTML || '0';
 
-    let novoProva1 = prompt("Nova nota da Prova 1:", prova1);
-    let novoAEP1 = prompt("Nova nota da AEP 1:", aep1);
-    let novoIntegrada1 = prompt("Nova nota da Prova Integrada 1:", integrada1);
-    let novoProva2 = prompt("Nova nota da Prova 2:", prova2);
-    let novoAEP2 = prompt("Nova nota da AEP 2:", aep2);
-    let novoIntegrada2 = prompt("Nova nota da Prova Integrada 2:", integrada2);
+    let novoProva1 = prompt("Nova nota da Prova 1:", prova1) || '0';
+    let novoAEP1 = prompt("Nova nota da AEP 1:", aep1) || '0';
+    let novoIntegrada1 = prompt("Nova nota da Prova Integrada 1:", integrada1) || '0';
+    let novoProva2 = prompt("Nova nota da Prova 2:", prova2) || '0';
+    let novoAEP2 = prompt("Nova nota da AEP 2:", aep2) || '0';
+    let novoIntegrada2 = prompt("Nova nota da Prova Integrada 2:", integrada2) || '0';
 
-    // Validar as novas notas
+    if (!validarNota(novoProva1) || !validarNota(novoAEP1) || !validarNota(novoIntegrada1) ||
+        !validarNota(novoProva2) || !validarNota(novoAEP2) || !validarNota(novoIntegrada2)) {
+        alert("As notas devem ser um número entre 0.0 e 10.0.");
+        return;
+    } else {
 
-    // Se todas as notas passaram na validação, atualizar as células da linha
-    linha.cells[3].innerHTML = novoProva1 || '-';
-    linha.cells[4].innerHTML = novoAEP1 || '-';
-    linha.cells[5].innerHTML = novoIntegrada1 || '-';
-    linha.cells[6].innerHTML = novoProva2 || '-';
-    linha.cells[7].innerHTML = novoAEP2 || '-';
-    linha.cells[8].innerHTML = novoIntegrada2 || '-';
+        // Atualizar as células da linha com as novas notas
+        linha.cells[3].innerHTML = novoProva1 || '-';
+        linha.cells[4].innerHTML = novoAEP1 || '-';
+        linha.cells[5].innerHTML = novoIntegrada1 || '-';
+        linha.cells[7].innerHTML = novoProva2 || '-';
+        linha.cells[8].innerHTML = novoAEP2 || '-';
+        linha.cells[9].innerHTML = novoIntegrada2 || '-';
 
-    // Atualizar as notas no objeto aluno
-    const ra = linha.cells[1].innerHTML;
-    const alunos = JSON.parse(localStorage.getItem('aluno')) || [];
-    const index = alunos.findIndex(aluno => aluno.ra === ra);
-    if (index !== -1) {
-        alunos[index].prova1 = novoProva1 || '-';
-        alunos[index].aep1 = novoAEP1 || '-';
-        alunos[index].prova_integrada1 = novoIntegrada1 || '-';
-        alunos[index].prova2 = novoProva2 || '-';
-        alunos[index].aep2 = novoAEP2 || '-';
-        alunos[index].prova_integrada2 = novoIntegrada2 || '-';
-        localStorage.setItem('aluno', JSON.stringify(alunos));
-    }
-
-    calcularMediaTotalProva1();
-    calcularMedia1Total();
-    calcularMediaTotalAEP1();
-    calcularMediaTotalIntegrada1();
-    calcularMediaTotalProva2();
-    calcularMedia2Total();
-    calcularMediaTotalAEP2();
-    calcularMediaTotalIntegrada2();
-    calcularMediaTotalFinal();
-}
-        
-
-
-    function excluirLinha(ra) {
-        let tabela = document.getElementById('idTabela');
-        let alunos = JSON.parse(localStorage.getItem('aluno')) || [];
-    
-        let index = alunos.findIndex(aluno => aluno.ra === ra);
-    
+        // Atualizar as notas no objeto aluno, se necessário
+        const ra = linha.cells[1].innerHTML;
+        const alunos = JSON.parse(localStorage.getItem('aluno')) || [];
+        const index = alunos.findIndex(aluno => aluno.ra === ra);
         if (index !== -1) {
-            alunos.splice(index, 1);
-    
+            alunos[index].prova1 = novoProva1 || '-';
+            alunos[index].aep1 = novoAEP1 || '-';
+            alunos[index].prova_integrada1 = novoIntegrada1 || '-';
+            alunos[index].prova2 = novoProva2 || '-';
+            alunos[index].aep2 = novoAEP2 || '-';
+            alunos[index].prova_integrada2 = novoIntegrada2 || '-';
+
+            let media1 = calcularMedia1(alunos[index]);
+            let media2 = calcularMedia2(alunos[index]);
+            let final = calcularMediaFinal(alunos[index]);
+            let situacao = definirSituacao(alunos[index]);
+
+            alunos[index].media1 = media1;
+            alunos[index].media2 = media2;
+            alunos[index].final = final;
+            alunos[index].situacao = situacao;
+
+            linha.cells[6].innerHTML = parseFloat(media1).toFixed(2); // Média 1
+            linha.cells[10].innerHTML = parseFloat(media2).toFixed(2); // Média 2
+            linha.cells[11].innerHTML = parseFloat(final).toFixed(2); // Média Final
+            linha.cells[12].innerHTML = situacao;
             localStorage.setItem('aluno', JSON.stringify(alunos));
-    
-            tabela.deleteRow(index + 1);
         }
+
+        // Chamar as funções para recalcular as médias totais
         calcularMediaTotalProva1();
         calcularMedia1Total();
         calcularMediaTotalAEP1();
@@ -326,6 +369,31 @@ function editarNotas(botaoAdd) {
         calcularMediaTotalIntegrada2();
         calcularMediaTotalFinal();
     }
+}
+
+function excluirLinha(ra) {
+    let tabela = document.getElementById('idTabela');
+    let alunos = JSON.parse(localStorage.getItem('aluno')) || [];
+
+    let index = alunos.findIndex(aluno => aluno.ra === ra);
+
+    if (index !== -1) {
+        alunos.splice(index, 1);
+
+        localStorage.setItem('aluno', JSON.stringify(alunos));
+
+        tabela.deleteRow(index + 1);
+    }
+    calcularMediaTotalProva1();
+    calcularMedia1Total();
+    calcularMediaTotalAEP1();
+    calcularMediaTotalIntegrada1();
+    calcularMediaTotalProva2();
+    calcularMedia2Total();
+    calcularMediaTotalAEP2();
+    calcularMediaTotalIntegrada2();
+    calcularMediaTotalFinal();
+}
 
 function configurarPopup() {
     limparCampos();
@@ -347,7 +415,6 @@ function configurarPopup() {
         let ra = document.getElementById("input_ra").value;
         let email = document.getElementById("input_email").value;
 
-
         /*const aluno = validarCadastro(nome, ra, email);
         if (aluno) {
             adicionaDadosAluno();
@@ -358,7 +425,6 @@ function configurarPopup() {
         } */
     });
 }
-
 
 function configurarPopupNotas(aluno) {
     limparCampos();
@@ -392,6 +458,7 @@ function configurarPopupNotas(aluno) {
         let aep2 = document.getElementById("input_aep_2").value;
         let prova_integrada2 = document.getElementById("input_prova_integrada_2").value;
 
+
         // Adiciona as notas do aluno
         adicionarNotasAluno(aluno, prova1, aep1, prova_integrada1, prova2, aep2, prova_integrada2);
         document.getElementById("popupNotas").classList.remove("show");
@@ -402,15 +469,13 @@ function configurarPopupNotas(aluno) {
     });
 }
 
-
-
 function limparCampos() {
     document.getElementById('input_nome').value = "";
     document.getElementById('input_ra').value = "";
     document.getElementById('input_email').value = "";
 }
 
-function limparNotas(){
+function limparNotas() {
     document.getElementById('input_prova_1').value = "";
     document.getElementById('input_aep_1').value = "";
     document.getElementById('input_prova_integrada_1').value = "";
@@ -423,53 +488,79 @@ function limparNotas(){
 // Cálculos de média
 
 function calcularMedia1(aluno) {
-    let prova = parseFloat(aluno.prova1) * 0.8;
-    let aep = parseFloat(aluno.aep1) * 0.1;
-    let integrada = parseFloat(aluno.prova_integrada1) * 0.1;
+    let prova1 = aluno.prova1; 
+    let aep1 = aluno.aep1;
+    let provaIntegrada1 = aluno.prova_integrada1;
 
-    let media = prova + aep + integrada;
-
-    if(isNaN(media)){
-        return 0;
-    }else{
-        return media.toFixed(1);
+    if (prova1 === "" || prova1 === null || prova1 === undefined || prova1 === "-") {
+        prova1 = 0.0;
+    } else if (aep1 === "" || aep1 === null || aep1 === undefined || aep1 === "-") {
+        aep1 = 0.0;
     }
+
+    // Verifica se provaIntegrada1 não está definido e atribui 0.0 caso verdadeiro
+    provaIntegrada1 = provaIntegrada1 || 0.0;
+
+    const media1 = ((parseFloat(prova1) * 0.8) + (parseFloat(aep1) * 0.1) + (parseFloat(provaIntegrada1) * 0.1));
+
+    // Se a média for NaN, retorna 0.0
+    if (isNaN(media1)) {
+        return '0.0';
+    }
+
+    return media1.toFixed(2);
 }
 
 function calcularMedia2(aluno) {
-    let prova = parseFloat(aluno.prova2) * 0.8;
-    let aep = parseFloat(aluno.aep2) * 0.1;
-    let integrada = parseFloat(aluno.prova_integrada2) * 0.1;
+    let prova2 = aluno.prova2;
+    let aep2 = aluno.aep2;
+    let provaIntegrada2 = aluno.prova_integrada2;
 
-    let media = prova + aep + integrada;
-
-    if(isNaN(media)){
-        return 0;
-    }else{
-        return media.toFixed(1);
+    if (prova2 === "" || prova2 === null || prova2 === undefined || prova2 === "-") {
+        prova2 = 0.0;
+    } else if (aep2 === "" || aep2 === null || aep2 === undefined || aep2 === "-") {
+        aep2 = 0.0;
     }
+
+    // Verifica se provaIntegrada2 não está definido e atribui 0.0 caso verdadeiro
+    provaIntegrada2 = provaIntegrada2 || 0.0;
+
+    const media2 = ((parseFloat(prova2) * 0.8) + (parseFloat(aep2) * 0.1) + (parseFloat(provaIntegrada2) * 0.1));
+
+    // Se a média for NaN, retorna 0.0
+    if (isNaN(media2)) {
+        return '0.0';
+    }
+
+    return media2.toFixed(2);
 }
+
+// function calcularMediaFinal(aluno) {
+//     const final = (parseFloat(aluno.media1) + parseFloat(aluno.media2)) / 2;
+//     return final.toFixed(2);
+// }
 
 function calcularMediaFinal(aluno) {
     let media1 = parseFloat(calcularMedia1(aluno)) || 0; // Define 0 se a média1 não puder ser convertida em um número
     let media2 = parseFloat(calcularMedia2(aluno)) || 0; // Define 0 se a média2 não puder ser convertida em um número
     let final = (media1 + media2) / 2;
 
-    return final.toFixed(1);
+    return final.toFixed(2);
 }
 
-
 function definirSituacao(aluno) {
+    let situacao = "";
     let media = calcularMediaFinal(aluno);
     if (media >= 6.0) {
-        return "Aprovado";
+        situacao = "Aprovado";
     }
     else if (media <= 3.0) {
-        return "Reprovado";
+        situacao = "Reprovado";
     }
     else {
-        return "Recuperacao";
+        situacao = "Recuperacao";
     }
+    return situacao;
 }
 
 function calcularMedia1Total() {
@@ -513,7 +604,6 @@ function calcularMediaTotalProva1() {
         document.getElementById("mediaProva1").textContent = "0.0"; // Caso não haja notas válidas
     }
 }
-
 
 function calcularMediaTotalAEP1() {
     const alunos = JSON.parse(localStorage.getItem('aluno')) || [];
@@ -691,5 +781,5 @@ function alternarBimestre() {
         document.getElementById("input_prova_2").value = "";
         document.getElementById("input_aep_2").value = "";
         document.getElementById("input_prova_integrada_2").value = "";
-    } 
+    }
 }
