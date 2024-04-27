2ae415154fabd23e772a66d7e9e438611dd563cc
Leonardo <leonardoposs99@gmail.com>
2024-04-26 21:28:06 -0300

    Excluindo o arquivo Alunos (Não utilizada)

--- assets/js/Aluno.js
+++
@@ -1,8 +0,0 @@
-// class Aluno {
-//     constructor(nome, ra, email){
-//         this.nome = nome;
-//         this.ra = ra;
-//         this.email = email;
-//         this.prova1 = prova1;
-//     }
-// }
